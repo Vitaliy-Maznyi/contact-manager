3bca3ad2d4d65b8df1f4e84494e881226dc33bad
VitaliyMaznyi <vitalii.maznyi@fasterthanlight.me>
2020-05-15 21:52:17 +0300

    add react app initial files to client folder

---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './App.module.scss'
+
+const App = () => (
+  <div className={styles.App}>
+    <h1>Contact manager app</h1>
+  </div>
+)
+
+export default App
