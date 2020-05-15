124e90b3e1cb64494d07cb78d7a1e41981507a2d
VitaliyMaznyi <vitalii.maznyi@fasterthanlight.me>
2020-05-16 00:16:43 +0300

    add contacts routes
    add index action

--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -1,5 +1,14 @@
-module Api
-  class ContactsController < Api::BaseController
+class Api::ContactsController < Api::BaseController
+  before_action :set_current_user
 
+  def index
+    @contacts = @current_user.contacts
+    render json: @contacts
+  end
+
+  private
+
+  def set_current_user
+    @current_user ||= User.first # TODO: remove after adding authentication
   end
 end
